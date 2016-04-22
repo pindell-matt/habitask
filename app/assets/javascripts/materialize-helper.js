cf2649c8f5db2b8ae5194d8e903b6ad33d73bf0c
Matt Pindell <pindell.m@gmail.com>
2016-04-21 18:14:43 -0600

    Update photos and styling on main page

--- app/assets/javascripts/materialize-helper.js
+++ app/assets/javascripts/materialize-helper.js
@@ -1,3 +1,11 @@
-$( document ).ready(function(){
-  $(".dropdown-button").dropdown();
-})
+$(document).ready(function(){
+  $('.slider').slider({Default: 100});
+  $(".dropdown-button").dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrain_width: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: true // Displays dropdown below the button
+  });
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def cities
+    City.all
+  end
 end
