5daca261fc9b9a404d8838173ed6901f5bb5e9b5
patrickwhardy <patrickwhardy@gmail.com>
2016-05-02 14:04:13 -0600

    hand write routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  resources :tasks, only: [:index, :show]
-  resources :cart_tasks, only: [:create, :destroy]
-  resources :volunteers, only: [:create, :new, :edit]
+  # resources :tasks, only: [:index, :show]
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  # resources :cart_tasks, only: [:create, :destroy]
+  post '/cart_tasks', to: 'cart_tasks#create'
+  delete '/cart_tasks/:id', to: 'cart_tasks#destroy', as: 'cart_task'
+
+  # resources :volunteers, only: [:create, :new, :edit]
+  post '/volunteers', to: 'volunteers#create'
+  get '/volunteers/new', to: 'volunteers#new', as: 'new_volunteer'
+  get '/volunteers/:id/edit', to: 'volunteers#edit', as: 'edit_volunteer'
 
   get '/dashboard', to: 'volunteers#show'
   get "/cart", to: 'cart_tasks#show'
