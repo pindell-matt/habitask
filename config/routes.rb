69d7c32fa2861ca053be6b4d50fb85aa7831ad25
Matt Pindell <pindell.m@gmail.com>
2016-05-03 20:11:01 -0600

    Update Routes for Intermission assigment

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,35 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  resources :tasks, only: [:index, :show]
-  resources :cart_tasks, only: [:create, :destroy]
-  resources :volunteers, only: [:create, :new, :edit]
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  post '/cart_tasks', to: 'cart_tasks#create'
+  delete '/cart_tasks/:id', to: 'cart_tasks#destroy', as: :cart_task
+
+  post '/volunteers', to: 'volunteers#create'
+  get '/volunteers/new', to: 'volunteers#new', as: :new_volunteer
+  get '/volunteers/:id/edit', to: 'volunteers#edit', as: :edit_volunteer
 
   get '/dashboard', to: 'volunteers#show'
-  get "/cart", to: 'cart_tasks#show'
+  get '/cart', to: 'cart_tasks#show'
 
   get '/commitments', to: 'commitments#show', as: :commitments
   post '/commitments', to: 'commitments#create'
 
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete "/logout", to: 'sessions#destroy'
 
-  get "/:name", to: 'cities#show', as: :city
+  get '/:name', to: 'cities#show', as: :city
 
-  namespace :admin do
-    get '/dashboard', to: 'volunteers#show'
-    patch '/dashboard', to: 'volunteers#update'
-    get '/edit', to: 'volunteers#edit'
-    resources :tasks, only: [:new, :create, :edit, :update]
-  end
+  get '/admin/dashboard', to: 'admin/volunteers#show', as: :admin_dashboard
+  patch '/admin/dashboard', to: 'admin/volunteers#update'
 
+  get '/admin/edit', to: 'admin/volunteers#edit', as: :admin_edit
+
+  get '/admin/tasks/new', to: 'admin/tasks#new', as: :new_admin_task
+  get '/admin/tasks/:id/edit', to: 'admin/tasks#edit', as: :edit_admin_task
+  post '/admin/tasks', to: 'admin/tasks#create', as: :admin_tasks
+  patch 'admin/tasks/:id', to: 'admin/tasks#update', as: :admin_task
 end
